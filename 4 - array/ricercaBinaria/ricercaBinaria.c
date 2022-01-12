d9734f711ba46574b8832582c6ec8301a6394b75
marmic98 <michelmartino98.mm@gmail.com>
2022-01-12 11:55:20 +0100

    update

--- 4 - array/ricercaBinaria/ricercaBinaria.c
+++ 4 - array/ricercaBinaria/ricercaBinaria.c
@@ -3,18 +3,17 @@
 #define N 8
 
 int ricercaBinaria(int* a,int item){
-    int inizio=0,fine=N-1,centro=fine/2;
+    int inizio=0,fine=N-1,centro;
     while(inizio <= fine){
+        centro = (inizio+inizio)/2;
         if (a[centro]==item){
             return centro;
         }
          else if (item > a[centro]){
             inizio = centro+1;
-            centro = (inizio+inizio)/2;
         }
         else {
             fine = centro-1;
-            centro = (inizio+inizio)/2;
         }
     }
     return -1;
@@ -25,10 +24,10 @@ int main(){
     int item;
     
     printf("Fornire elemento da ricercare: ");
-    scanf("%d",&item);
+    scanf("%d", &item);
 
     int a[N] = {0,5,10,15,91,115,215,366};
-    int found = ricercaBinaria(a,item);	
+    int pos = ricercaBinaria(a,item);	
 
-    found == -1 ? printf("L'elemento %d non è presente nell'array\n",item) : printf("L'elemento %d e' in posizione %d\n",item,found);
+    pos == -1 ? printf("L'elemento %d non è presente nell'array\n",item) : printf("L'elemento %d e' in posizione %d\n",item,pos);
 }
